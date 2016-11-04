fd62c0f3f54e96a9dc71ccc237f97ae6159ed09e
刘沃劲 <gzliuwojin@corp.netease.com>
2016-11-04 09:32:29 +0800

    第一次运行，耗时10h51min

--- beautyM.py
+++ beautyM.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 
 
@@ -9,8 +10,9 @@ def main():
     url = "http://www.beautylegmm.com"
     ablumlist = []
     piclist = []
-    i = 0
-    while(i<1):
+    i = 1
+
+    while(i):
         list,nurl = parse_ablum(url)
         ablumlist.extend(list)
         if nurl is not None:
@@ -18,31 +20,69 @@ def main():
         else:
             break
         i +=1
+        print("ablum %d" % i)
+    print(time.ctime())
 
-    for i in ablumlist:
-        url = i.a["href"]
+    with open("ablumlist.txt","w") as ablumlist_file:
+        ablumlist_file.writelines(["%s\n" % item for item in ablumlist])
+
+    s = 1
+    errorPage_list =[]
+    for h in ablumlist:
+        url = h.a["href"]
 
         while(True):
-            alist,nurl= parse_pic(url)
-            piclist.extend(alist)
+            try:
+                alist,nurl= parse_pic(url)
+                piclist.extend(alist)
+                time.sleep(0.5)
+                s += 1
+                print("pic %d" % s)
+            except:
+                errorPage_list.extend(url)
+                break
+
             if nurl is not None:
                 url = nurl
             else:
                 break
+    print("end of pic")
 
-    print(piclist)
+    with open("m.txt","w") as f:
+        f.writelines(["%s\n" % item for item in piclist])
 
 
 def parse_ablum(url):
-    respone = requests.get(url)
+    r = requests.session()
+    # r.keep_alive = False
+    r.adapters.DEFAULT_RETRIES = 2000
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
+        "Connection": "keep-alive",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    }
+    respone = r.get(url,headers = header)
     soup = BeautifulSoup(respone.text, "html.parser")
     list = soup.find_all(attrs={"class": "post_weidaopic"})
-    nurl =  soup.find(attrs={"class":"next"}).a["href"]
+    # nurl = None
+    try:
+        nurl =  soup.find(attrs={"class":"next"}).a["href"]
+    except:
+        nurl = None
+    r.close()
     return  list,nurl
 
 
 def parse_pic(url):
-    respone = requests.get(url)
+    r = requests.session()
+    # r.keep_alive = False
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
+        "Connection": "keep-alive",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    }
+    r.adapters.DEFAULT_RETRIES = 10
+    respone = r.get(url,headers = header)
     soup = BeautifulSoup(respone.text,"html.parser")
     piclist = soup.find_all(has_real_pic)
     next_url = None
@@ -50,9 +90,11 @@ def parse_pic(url):
         next_url = soup.find(attrs={"class": "next"})["href"]
     except:
         next_url = None
-
+    r.close()
     return piclist,next_url
 
 
 if __name__ == '__main__':
+    print("starttime %s" % time.ctime())
     main()
+    print("endtime %s " % time.ctime())
